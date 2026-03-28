f78982d6448bad5fb5c6f0bb7cf9909353b0fc10
admin <admin@exmaple.com>
2026-03-28 17:13:35 +0100

    upgrade

--- api/start-api.sh
+++ api/start-api.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 set -e
 
-alembic upgrade head
 exec uvicorn main:app --host 0.0.0.0 --port 80
--- api/start-worker.sh
+++ api/start-worker.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 set -e
 
-alembic upgrade head
 exec taskiq worker taskiq_broker:broker tasks
