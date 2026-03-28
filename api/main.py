1bc00eae8b8f85386c0759b4cd8fb837fb50dfcb
admin <admin@exmaple.com>
2026-03-28 18:31:07 +0100

    fix cors

--- api/main.py
+++ api/main.py
@@ -31,6 +31,8 @@ app.add_middleware(
         'http://localhost:8000',
         'http://127.0.0.1:5173',
         'http://localhost:5173',
+        'https://app.gemellus.app',
+        '*',
     ],
     allow_credentials=True,
     allow_methods=['*'],
